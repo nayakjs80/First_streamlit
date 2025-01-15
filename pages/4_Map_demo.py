c41caac5e7e3eb458a97f5702f63de84571f371d
nayakjs80 <39116743+nayakjs80@users.noreply.github.com>
2025-01-15 14:31:00 +0900

    Add files via upload

---
+++ pages/4_Map_demo.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import folium
+from geopy.geocoders import Nominatim
+from streamlit_folium import folium_static
+
+
+st.set_page_config(page_title="Display Self", page_icon="")
+
+st.markdown("Display Self")
+
+
+try:
+    # 사용자 에이전트 설정
+    geolocator = Nominatim(user_agent="my_app")
+
+    # 내 위치를 가져오기
+    location = geolocator.geocode("경기도, 대한민국")
+
+    # 지도의 중심을 내 위치로 설정
+    map = folium.Map(location=[location.latitude, location.longitude], zoom_start=15)
+
+    # 내 위치에 마커 추가
+    folium.Marker([location.latitude, location.longitude], popup="내 위치").add_to(map)
+
+    # Streamlit 앱 설정
+    st.title("내 위치 지도")
+    st.write("아래 지도에서 내 위치를 확인하세요:")
+
+    # 지도를 Streamlit 앱에 표시
+    folium_static(map)
+
+
+    # # 내 위치를 가져오기 위한 Geolocator 설정
+    # geolocator = Nominatim(user_agent="geoapiExercises")
+    # location = geolocator.geocode("경기도, 대한민국")
+
+    # # 지도의 중심을 내 위치로 설정
+    # map = folium.Map(location=[location.latitude, location.longitude], zoom_start=15)
+
+    # # 내 위치에 마커 추가
+    # folium.Marker([location.latitude, location.longitude], popup="내 위치").add_to(map)
+
+    # # Streamlit 앱 설정
+    # st.title("내 위치 지도")
+    # st.write("아래 지도에서 내 위치를 확인하세요:")
+
+    # # 지도를 Streamlit 앱에 표시
+    # folium_static(map)
+
+except RuntimeError as e:
+    st.error(
+        """
+        **This demo requires internet access.**
+
+        Connection error: %s
+    """
+        % e.reason
+    )
+
+# import streamlit as st
+# import folium
+# from geopy.geocoders import Nominatim
+# from streamlit_folium import folium_static
+
+# # 사용자 에이전트 설정
+# geolocator = Nominatim(user_agent="my_app")
+
+# # 내 위치를 가져오기
+# location = geolocator.geocode("경기도, 대한민국")
+
+# # 지도의 중심을 내 위치로 설정
+# map = folium.Map(location=[location.latitude, location.longitude], zoom_start=15)
+
+# # 내 위치에 마커 추가
+# folium.Marker([location.latitude, location.longitude], popup="내 위치").add_to(map)
+
+# # Streamlit 앱 설정
+# st.title("내 위치 지도")
+# st.write("아래 지도에서 내 위치를 확인하세요:")
+
+# # 지도를 Streamlit 앱에 표시
+# folium_static(map)
